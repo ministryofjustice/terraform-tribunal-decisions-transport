5aa1c02b818c7a36c119df9d09c50a055996fe5a
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-01-31 12:46:44 +0000

    new secrets added, mssql user creation added

--- rds.tf
+++ rds.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    mssql = {
+      source = "betr-io/mssql"
+      version = "0.1.0"
+    }
+  }
+}
+
+provider "mssql" {
+  debug = "false"
+}
+
 resource "null_resource" "setup_db" {
   depends_on = [data.aws_db_instance.database] #wait for the db to be ready
 
@@ -12,4 +25,54 @@ resource "null_resource" "setup_db" {
   triggers = {
     always_run = "${timestamp()}"
   }
+}
+
+ resource "random_password" "password" {
+  length  = 16
+  special = true //Only printable ASCII characters besides '/', '@', '"', ' ' may be used.
+  override_special = "!#$%&*()-_=+[]{}<>:?" 
+}
+
+data "aws_secretsmanager_secret" "db-secrets" {
+  arn = "arn:aws:secretsmanager:eu-west-2:207640118376:secret:tf-tribunals-dev-credentials-Hxjqy2"
+}
+
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.db-secrets.id
+}
+
+
+resource "mssql_user" "db-user" {
+  server {
+    host = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["host"]    
+    port = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["port"]
+    login {
+      username = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["username"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["password"]
+    }
+  }
+
+  database  = "transport"
+  username  = "admin"
+  password  = random_password.password.result 
+
+  roles     = ["db_owner"]
+}
+
+
+ resource "aws_secretsmanager_secret" "rds_credentials" {
+  name = "tf-tribunals-${var.application_name}-${var.environment}-credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials" {
+  secret_id     = aws_secretsmanager_secret.rds_credentials.id
+  secret_string = <<EOF
+{
+  "username": "${mssql_user.db-user.username}",
+  "password": "${mssql_user.db-user.password}",  
+  "host": "${mssql_user.db-user.server[0].host}",
+  "port": ${mssql_user.db-user.server[0].port}, 
+  "database_name": ${mssql_user.db-user.database}
+}
+EOF
 }
