e526b8b831cbad293795228dbe2eab13d7469bd8
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-02-14 16:04:18 +0000

    dms target hostname fixed

--- dms.tf
+++ dms.tf
@@ -2,9 +2,9 @@ module "dms" {
   source = "github.com/ministryofjustice/terraform-tribunals-dms?ref=master"
 
   db_instance                       = data.aws_db_instance.database
-  db_hostname                       = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["username"]
+  db_hostname                       = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["host"]
   db_password                       = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["password"]
-  db_username                       = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["host"]
+  db_username                       = jsondecode(data.aws_secretsmanager_secret_version.rds.secret_string)["username"]
   dms_source_db_password            = jsondecode(data.aws_secretsmanager_secret_version.source-db.secret_string)["password"]
   dms_source_db_username            = jsondecode(data.aws_secretsmanager_secret_version.source-db.secret_string)["username"]
   dms_source_db_hostname            = jsondecode(data.aws_secretsmanager_secret_version.source-db.secret_string)["host"]
