acedbfcdffaf94ada93ad55f9a5aaaa93f8beccc
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-02-15 01:17:29 +0000

    dms source settings added

--- dms.tf
+++ dms.tf
@@ -8,10 +8,10 @@ module "dms" {
   dms_source_db_password            = jsondecode(data.aws_secretsmanager_secret_version.source-db.secret_string)["password"]
   dms_source_db_username            = jsondecode(data.aws_secretsmanager_secret_version.source-db.secret_string)["username"]
   dms_source_db_hostname            = jsondecode(data.aws_secretsmanager_secret_version.source-db.secret_string)["host"]
-  #dms_replication_instance          = module.dms.dms_replication_instance
+  dms_replication_instance          = module.dms.dms_replication_instance
   region                            = var.region
-  #dms_target_account_access_key     = var.dms_target_account_access_key
-  #dms_target_account_secret_key     = var.dms_target_account_secret_key
+  dms_source_account_access_key     = jsondecode(data.aws_secretsmanager_secret_version.source-db.secret_string)["dms_source_account_access_key"]
+  dms_source_account_secret_key     = jsondecode(data.aws_secretsmanager_secret_version.source-db.secret_string)["dms_source_account_secret_key"]
   application_name                  = var.application_name
   source_db_name                    = "Transport"
   target_db_name                    = "transport"
