f2f5ba54798692c054ba44bac43a8991045258a8
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-02-17 10:14:43 +0000

    aws source account security group creation bash file added

--- elasticBeanStalk.tf
+++ elasticBeanStalk.tf
@@ -1,12 +1,12 @@
-module "elasticBeanStalk" {
-    source = "github.com/ministryofjustice/terraform-tribunals-elasticbeanstalk.git?ref=main"  
+# module "elasticBeanStalk" {
+#     source = "github.com/ministryofjustice/terraform-tribunals-elasticbeanstalk.git?ref=main"  
 
-certificate                      = module.dns_Ssl.certificate
-application_name                 = local.name
-instance_type                    = var.instance_type
-vpc_id                           = data.aws_vpc.selected.id
-environment                      = var.environment
-#vpc_security_group_id            = aws_security_group.security_group.id
-subnet_ids                       = [data.aws_subnet.subnet2a.id, data.aws_subnet.subnet2b.id, data.aws_subnet.subnet2c.id]
+# certificate                      = module.dns_Ssl.certificate
+# application_name                 = local.name
+# instance_type                    = var.instance_type
+# vpc_id                           = data.aws_vpc.selected.id
+# environment                      = var.environment
+# #vpc_security_group_id            = aws_security_group.security_group.id
+# subnet_ids                       = [data.aws_subnet.subnet2a.id, data.aws_subnet.subnet2b.id, data.aws_subnet.subnet2c.id]
 
-}
+# }
