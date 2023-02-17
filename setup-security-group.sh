8cc1fcc6f5cbcaa73352b3a59739bef4294d1925
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-02-17 11:47:43 +0000

    region info added into bash

--- setup-security-group.sh
+++ setup-security-group.sh
@@ -8,12 +8,12 @@ aws configure set region "$AWS_REGION" --profile dts-legacy-apps-user
 aws configure set output "json" --profile dts-legacy-apps-user
 
 #retrieve existing security groups 
-aws ec2 describe-instances --instance-ids ${EC2_INSTANCE_ID} --profile dts-legacy-apps-user --query "Reservations[].Instances[].SecurityGroups[].GroupId" > security_groups
+aws ec2 describe-instances --region ${AWS_REGION} --instance-ids ${EC2_INSTANCE_ID} --profile dts-legacy-apps-user --query "Reservations[].Instances[].SecurityGroups[].GroupId" > security_groups
 
 existing_group_ids="$(sed 's/[^a-zA-Z0-9-]//g' security_groups)"
 echo existing group ids : $existing_group_ids
 echo DMS security group id : ${DMS_SECURITY_GROUP}
-aws ec2 modify-instance-attribute --instance-id ${EC2_INSTANCE_ID} --groups $existing_group_ids ${DMS_SECURITY_GROUP}
+aws ec2 modify-instance-attribute --region ${AWS_REGION} --instance-id ${EC2_INSTANCE_ID} --groups $existing_group_ids ${DMS_SECURITY_GROUP}
 
-aws ec2 describe-instances --instance-ids ${EC2_INSTANCE_ID} --profile dts-legacy-apps-user --query "Reservations[].Instances[].SecurityGroups[].GroupId" > security_groups
+aws ec2 describe-instances --region ${AWS_REGION} --instance-ids ${EC2_INSTANCE_ID} --profile dts-legacy-apps-user --query "Reservations[].Instances[].SecurityGroups[].GroupId" > security_groups
 cat security_groups
