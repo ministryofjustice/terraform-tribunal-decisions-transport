c686ab92da63e46276ce0ecf3ba8846a7717ea7a
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-02-23 00:50:49 +0000

    db security group for elasticbeanstalk

--- elasticBeanStalk.tf
+++ elasticBeanStalk.tf
@@ -6,7 +6,7 @@ application_name                 = local.name
 instance_type                    = var.instance_type
 vpc_id                           = data.aws_vpc.selected.id
 environment                      = var.environment
-#vpc_security_group_id            = aws_security_group.security_group.id
+vpc_security_group_id            = data.aws_db_instance.database.db_security_groups
 public_subnet_ids                = [data.aws_subnet.public_subnet2a.id, data.aws_subnet.public_subnet2b.id, data.aws_subnet.public_subnet2c.id]
 private_subnet_ids               = [data.aws_subnet.private_subnet2a.id, data.aws_subnet.private_subnet2b.id, data.aws_subnet.private_subnet2c.id]
 }
